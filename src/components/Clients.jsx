8448b89361d355b04b678ca8d22367397e6bfbaf
xander1421 <alexpruteanu@hotmai.com>
2023-04-22 00:06:13 +0300

    Revert "try  lazy-load images"
    This reverts commit bacc71194635257e03bfe659c60f0055bd19003d.

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,6 @@
 import { clients } from "../constants";
 import styles from "../style";
 import { useTranslation } from 'react-i18next';
-import LazyImage from './LazyImage';
 
 const Clients = () => {
   const { t, i18n } = useTranslation();
@@ -10,7 +9,7 @@ const Clients = () => {
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) => (
           <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
-            <LazyImage src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
+            <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
           </div>
         ))}
       </div>
