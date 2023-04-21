bacc71194635257e03bfe659c60f0055bd19003d
xander1421 <alexpruteanu@hotmai.com>
2023-04-22 00:02:26 +0300

    try  lazy-load images

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,6 +1,7 @@
 import { clients } from "../constants";
 import styles from "../style";
 import { useTranslation } from 'react-i18next';
+import LazyImage from './LazyImage';
 
 const Clients = () => {
   const { t, i18n } = useTranslation();
@@ -9,7 +10,7 @@ const Clients = () => {
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) => (
           <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
-            <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
+            <LazyImage src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
           </div>
         ))}
       </div>
