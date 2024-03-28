a0ce71f0c54f0dc082322016b33e0953a15d3314
xander1421 <36821963+xander1421@users.noreply.github.com>
2024-03-28 21:08:13 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import React, { Suspense } from 'react';
-
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-// import i18n (needs to be bundled ;))
-import './i18n'
+import './i18n';
 
+// Define a Loading component using TailwindCSS classes
+const Loading = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-black text-white">
+      ...is loading
+    </div>
+  );
+};
+
+// Use the Loading component as the fallback
 ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
+  <React.StrictMode>
+    <Suspense fallback={<Loading />}>
       <App />
-</React.StrictMode>
-
+    </Suspense>
+  </React.StrictMode>
 );
